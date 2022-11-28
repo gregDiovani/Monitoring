b481437c8aa49ced411a1089abb0c98c7bdd7d6c
gregDiovani <83853641+gregDiovani@users.noreply.github.com>
2022-11-29 03:11:15 +0700

    Revert "PHP QRIS Dashboard V 1.2"
    This reverts commit d2ccd8d0c89de2afc76562fba810c8a119499e2a.

--- app/View/Home/history.php
+++ app/View/Home/history.php
@@ -73,20 +73,19 @@
                         <canvas id="spurChartjsBar"></canvas>
                         <script>
 
-                            const xValues1 = [<?php foreach ($model['data']['chart']  as $row) { echo '"' . $row['BulanName'] . '",'; } ?> ];
+                            const xValues1 = [<?php foreach ($model['data']['chart']  as $row) { echo '"' . date("M", strtotime($row['time'])) . '",'; } ?> ];
+
                             var ctx = document.getElementById("spurChartjsBar").getContext('2d');
                             var myChart = new Chart(ctx, {
-                                type: 'line',
+                                type: 'bar',
                                 data: {
                                     labels: xValues1,
-
                                     datasets: [
                                         {
                                             label: 'Pendapatan',
                                             data: [<?php foreach ($model['data']['chart']  as $row) { echo '"' . $row["totalRp"] . '",'; } ?>],
-                                            backgroundColor: '#9BD0F5',
+                                            backgroundColor: window.chartColors.primary,
                                             borderColor: 'transparent'
-
                                         },
 
                                     ]
