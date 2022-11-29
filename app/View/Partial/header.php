08468e2876c1f71b426b1c31fdc89b492537a608
gregDiovani <83853641+gregDiovani@users.noreply.github.com>
2022-11-29 11:17:48 +0700

    Revert "PHP QRIS Dashboard  V.13"
    This reverts commit 49ee95ac60e3424008c428c1668e6458d1ac1ca5.

--- app/View/Partial/header.php
+++ app/View/Partial/header.php
@@ -9,7 +9,7 @@
 
 <head>
     <meta charset="utf-8">
-    <link rel="shortcut icon" href="../public/css/favicon.ico">
+    <link rel="shortcut icon" href="/css/favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Nunito:400,600|Open+Sans:400,600,700" rel="stylesheet">
@@ -19,16 +19,16 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css" />
 
     <!--Spur table -->
-    <link rel="stylesheet" href="../public/css/spur.css">
+    <link rel="stylesheet" href="/css/spur.css">
 
 
 
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.bundle.min.js"></script>
-    <script src="../public/js/chart-js-config.js"></script>
+    <script src="/js/chart-js-config.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="../public/js/spur.js"></script>
+    <script src="/js/spur.js"></script>
     <!--Popper -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 
--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use Gregorio\App\Router;
 use Gregorio\Controller\HomeController;
